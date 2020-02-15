9ec6ac72ee038963c07d6d28fb0552b946c2ec50
Gabriel Reiser <gabe@gabereiser.com>
2020-02-15 08:35:25 -0700

    Updated types to remove SDL

--- include/types.h
+++ include/types.h
@@ -6,33 +6,18 @@
 #include <stddef.h>
 #include <iostream>
 
-#include <SDL2/SDL.h>
-#include <SDL_ttf.h>
-#include <SDL_image.h>
-#include <SDL_net.h>
+#include "bgfx/bgfx.h"
 
-#ifdef __APPLE__
-#include <OpenGL/gl.h>
-#endif
-
-#ifdef __linux__
-#include <gl.h>
-#endif
-
-#ifdef __WIN32__
-#include <gl.h>
-#endif
-
-#include <glm/glm.hpp>
 #include "config.h"
 
-using vec2    = glm::vec2;
-using vec3    = glm::vec3;
-using vec4    = glm::vec4;
-using quat    = glm::quat;
-using matrix  = glm::mat4;
-using matrix3 = glm::mat3;
-using matrix2 = glm::mat2;
+#if defined(_MSC_VER)
+    //  Microsoft 
+    #define API __declspec(dllexport)
+    #define IMPORT __declspec(dllimport)
+#else
+    #define API __attribute__((visibility("default")))
+#endif
+
 
 namespace rtech {
     
--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,9 @@
 #include "types.h"
 
-int main(int argc, char** argv) {
+API int main(int argc, char** argv) {
     std::cout << "rTech : ";
     std::cout << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << std::endl;
 
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
     
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
-
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-    }
-    else
-    {
-        int flags = SDL_WINDOW_OPENGL;
-        //Create window
-        window = SDL_CreateWindow( "rTech Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, flags);
-        if( window == NULL )
-        {
-            std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        }
-        else
-        {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-
-            //Fill the surface black
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0x00, 0x00, 0x00 ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
-            SDL_Event e;
-            bool quit = false;
-            while (!quit){
-                while (SDL_PollEvent(&e)){
-                    if (e.type == SDL_QUIT){
-                        quit = true;
-                    }
-                    if (e.type == SDL_KEYDOWN){
-                        quit = true;
-                    }
-                    if (e.type == SDL_MOUSEBUTTONDOWN){
-                        quit = true;
-                    }
-                    SDL_UpdateWindowSurface(window);
-                }
-            }
-        }
-    }
-    //Destroy window
-    SDL_DestroyWindow( window );
-
-    //Quit SDL subsystems
-    SDL_Quit();
     return 0;
 }
