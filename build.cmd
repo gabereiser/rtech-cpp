1854796e3d9863bb17a159b4fae2b1775b465da6
Gabriel Reiser <gabe@gabereiser.com>
2020-02-16 12:11:55 -0700

    Update build.cmd on windows to use makefiles

--- build.cmd
+++ build.cmd
@@ -5,8 +5,8 @@ git submodule update --init --recursive
 
 mkdir out
 cd out
-cmake -D CMAKE_INSTALL_PREFIX="./install" -DBGFX_BUILD_TOOLS=OFF ^
+cmake -G "Unix makefiles" -D CMAKE_INSTALL_PREFIX="./install" -DBGFX_BUILD_TOOLS=OFF ^
 -DBGFX_BUILD_EXAMPLES=OFF -DBGFX_INSTALL=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE ^
 ..
 dir
-msbuild rtech.vxproj /p:configuration=debug
+
