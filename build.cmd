14f0f780b9034e9ba48f3ca0ba869d4199fb3ac3
Gabriel Reiser <gabe@gabereiser.com>
2020-02-16 10:42:39 -0700

    Updated windows build.cmd

--- build.cmd
+++ build.cmd
@@ -3,13 +3,10 @@ set BUILD_TYPE=Debug
 
 git submodule update --init --recursive
 
-mkdir ./out
-cd ./out
-cmake -D CMAKE_INSTALL_PREFIX="./install" ^
-      -DBGFX_BUILD_TOOLS=OFF ^
-      -DBGFX_BUILD_EXAMPLES=OFF ^
-      -DBGFX_INSTALL=OFF ^
-      -DCMAKE_BUILD_TYPE=$BUILD_TYPE ^
-       ..
+mkdir out
+cd out
+cmake -D CMAKE_INSTALL_PREFIX="./install" -DBGFX_BUILD_TOOLS=OFF ^
+-DBGFX_BUILD_EXAMPLES=OFF -DBGFX_INSTALL=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE ^
+..
 make -j 
 make install
