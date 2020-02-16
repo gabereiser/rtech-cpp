5b51c3d643258d31e10124d2b237f6a998c106c7
Gabriel Reiser <gabe@gabereiser.com>
2020-02-16 10:20:23 -0700

    Adding build.cmd for windows

---
+++ build.cmd
@@ -0,0 +1,15 @@
+@echo off
+set BUILD_TYPE=Debug 
+
+git submodule update --init --recursive
+rmdir /s out
+mkdir out
+cd out
+cmake -D CMAKE_INSTALL_PREFIX="./install" \
+      -DBGFX_BUILD_TOOLS=OFF \
+      -DBGFX_BUILD_EXAMPLES=OFF \
+      -DBGFX_INSTALL=OFF \
+      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+       ..
+make -j 
+make install
