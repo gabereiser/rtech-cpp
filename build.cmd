5decf6d95c09f6fb4c54035e01d0edae61487ce4
Gabriel Reiser <gabe@gabereiser.com>
2020-02-16 12:36:42 -0700

    Fix win64 build : include std::function

--- build.cmd
+++ build.cmd
@@ -9,4 +9,4 @@ cmake -D CMAKE_INSTALL_PREFIX="./install" -DBGFX_BUILD_TOOLS=OFF ^
 -DBGFX_BUILD_EXAMPLES=OFF -DBGFX_INSTALL=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE ^
 ..
 dir
-msbuild rtech-bin.sln /p:configuration=%BUILD_TYPE%;platform=x64
+msbuild rtech-bin.sln /p:configuration=%BUILD_TYPE% /p:Platform=x64
