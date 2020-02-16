01208ae52ff95c037390d8595040d334fd405938
Gabriel Reiser <gabe@gabereiser.com>
2020-02-16 12:02:46 -0700

    Update build.cmd on windows to use devenv instead of makefiles

--- build.cmd
+++ build.cmd
@@ -5,7 +5,7 @@ git submodule update --init --recursive
 
 mkdir out
 cd out
-cmake -G "Unix makefiles" -D CMAKE_INSTALL_PREFIX="./install" -DBGFX_BUILD_TOOLS=OFF ^
+cmake -D CMAKE_INSTALL_PREFIX="./install" -DBGFX_BUILD_TOOLS=OFF ^
 -DBGFX_BUILD_EXAMPLES=OFF -DBGFX_INSTALL=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE ^
 ..
 devenv rtech.sln /build
